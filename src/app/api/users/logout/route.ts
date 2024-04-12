fd85aed763f8cae0cae000fd78e098da84a7b8d9
gauravver13 <gauravver13@gmail.com>
2024-04-13 02:40:46 +0530

    added logout routes

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,2 +1,25 @@
+import { connect } from "@/dbConfig/dbConfig";
+import  User  from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt  from "jsonwebtoken"
 
+connect()
 
+export async function POST(request:  NextRequest){
+    try {
+        const response = NextResponse.json({
+            message: "Logout Successfully",
+            success: true
+        })
+
+        response.cookies.set("token", "", {
+            httpOnly:true
+        })
+        
+        
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},
+            {status: 500})
+    }
+}
